7aa34d5ccf61d18a4a71a19a6b213f0aa3d6ed02
shet1990 <shet1990@gmail.com>
2018-09-30 16:55:16 +0300

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    let sumZero = 0;
+    let countZero;
+
+    for (n = 1; countZero != 0; n++) {
+        countZero = Math.floor(number / Math.pow(5, n));
+        sumZero = sumZero + countZero;
+    }
+    return sumZero;
 }
